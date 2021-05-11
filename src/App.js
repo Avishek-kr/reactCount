9db1dc69e00e9665692648cb934cc016ed9bf5a5
Abhishek Kumar <ak251997@gmail.com>
2021-05-11 21:23:45 +0530

    created 3 buttons with increase, reset and decrease.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  const [count, setCount] = useState(20);
+  const [count, setCount] = useState(0);
 
   // const stateChange =()=>{
   //   setCount(count+1);
@@ -10,12 +10,36 @@ export default function App() {
 
   return (
     <div className="App">
-      <h1>Hello Aashi Baby !! ✋</h1>
+      <h1>Hello There !! ✋</h1>
       <h2>My Count is {count} ! </h2>
+      <button
+        onClick={() => setCount(count - 1)}
+        style={{
+          backgroundColor: "red",
+          padding: "10px 20px",
+          border: "transparent",
+          fontWeight: "700",
+          cursor: "pointer"
+        }}
+      >
+        Decrease
+      </button>
+      <button
+        onClick={() => setCount(0)}
+        style={{
+          backgroundColor: "blue",
+          padding: "10px 20px",
+          border: "transparent",
+          fontWeight: "700",
+          cursor: "pointer"
+        }}
+      >
+        Reset
+      </button>
       <button
         onClick={() => setCount(count + 1)}
         style={{
-          backgroundColor: "blue",
+          backgroundColor: "lightgreen",
           padding: "10px 20px",
           border: "transparent",
           fontWeight: "700",
